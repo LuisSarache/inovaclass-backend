2d2e64340f2d1ca3e31c5a0e59fe03a333b7c655
YoneS2bwa <luisotavioss20001@gmail.com>
2025-08-04 21:02:15 -0300

    update

--- models/aluno.js
+++ models/aluno.js
@@ -1,7 +1,46 @@
+// src/models/aluno.js
+
 module.exports = (sequelize, DataTypes) => {
-  const Turma = sequelize.define("Turma", {
-    nome: DataTypes.STRING,
-    descricao: DataTypes.STRING
-  });
-  return Turma;
+  const Aluno = sequelize.define(
+    "Aluno",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "O nome do aluno não pode ser vazio" },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: { msg: "Informe um e-mail válido" },
+        },
+      },
+      senha: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "alunos",  // nome da tabela
+      timestamps: true,
+      underscored: true,
+    }
+  );
+
+  // Caso queira adicionar associações depois:
+  Aluno.associate = (models) => {
+    // Exemplo:
+    // Aluno.belongsTo(models.Turma, { foreignKey: "turma_id" });
+  };
+
+  return Aluno;
 };
---
+++ models/turma.js
@@ -0,0 +1,7 @@
+module.exports = (sequelize, DataTypes) => {
+  const Turma = sequelize.define("Turma", {
+    nome: DataTypes.STRING,
+    descricao: DataTypes.STRING
+  });
+  return Turma;
+};
