3a6a37d1d50386d7797e6db99143f484d6fcfd97
YoneS2bwa <luisotavioss20001@gmail.com>
2025-08-04 22:55:26 -0300

    Update message.js

--- models/message.js
+++ models/message.js
@@ -1,13 +1,22 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database'); // Corrigidoo
+// models/message.js
+module.exports = (sequelize, DataTypes) => {
+  const Message = sequelize.define('Message', {
+    autor: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    texto: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    timestamps: true,
+    updatedAt: false
+  });
 
-const Message = sequelize.define('Message', {
-  autor: { type: DataTypes.STRING, allowNull: false },
-  texto: { type: DataTypes.TEXT, allowNull: false },
-  createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
-}, {
-  timestamps: true,
-  updatedAt: false
-});
-
-module.exports = Message;
+  return Message;
+};
