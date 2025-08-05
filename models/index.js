6e7c49dc21487197e10253f399e6ab8279947e3b
YoneS2bwa <luisotavioss20001@gmail.com>
2025-08-04 21:21:07 -0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,33 +1,20 @@
 const { DataTypes } = require("sequelize");
-const { sequelize } = require("../config/database"); 
+const { sequelize } = require("../config/database"); // usa conexão já criada
 
-// Inicializa a conexão com Sequelize usando variáveis do .env
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || "mysql", // mysql, postgres, sqlite
-    logging: false, // false para não poluir o console
-  }
-);
-
-// Importa os models
+// Importa os models usando a mesma instância sequelize
 const Admin = require("./admin")(sequelize, DataTypes);
 const Professor = require("./professor")(sequelize, DataTypes);
 const Aluno = require("./aluno")(sequelize, DataTypes);
 const Turma = require("./turma")(sequelize, DataTypes);
 
-// Definir associações entre models (se existirem)
+// Define associações, se existirem
 if (Professor.associate) Professor.associate({ Admin, Aluno, Turma });
 if (Aluno.associate) Aluno.associate({ Admin, Professor, Turma });
 if (Turma.associate) Turma.associate({ Admin, Professor, Aluno });
 
-// Exporta para usar em qualquer lugar
+// Exporta para o resto da aplicação
 module.exports = {
   sequelize,
-  Sequelize,
   Admin,
   Professor,
   Aluno,
